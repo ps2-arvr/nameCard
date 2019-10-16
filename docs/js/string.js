14ca8ae1b1dfb681d2e686a52caf1e9efd9346c9
ps2-arvr <progressive.system2@gmail.com>
2019-10-16 17:27:39 +0900

    fix String class

--- docs/js/string.js
+++ docs/js/string.js
@@ -3,31 +3,30 @@ class String{
 	}
 	
 	getValue(param){
-		var array = getQueryString();
+		var array = this.getQueryString();
 		if(array == null){
 			return null;
 		}
 		return array[param];
 	}
-}
 
-
-function getQueryString(){
-	if (1 < document.location.search.length) {
-		// 最初の1文字 (?記号) を除いた文字列を取得する
-		var query = document.location.search.substring(1);
-		// クエリの区切り記号 (&) で文字列を配列に分割する
-		var parameters = query.split('&');
-		var result = new Object();
-		for (var i = 0; i < parameters.length; i++) {
-		// パラメータ名とパラメータ値に分割する
-			var element = parameters[i].split('=');
-			var paramName = decodeURIComponent(element[0]);
-			var paramValue = decodeURIComponent(element[1]);
-			// パラメータ名をキーとして連想配列に追加する
-			result[paramName] = paramValue;
+	getQueryString(){
+		if (1 < document.location.search.length) {
+			// 最初の1文字 (?記号) を除いた文字列を取得する
+			var query = document.location.search.substring(1);
+			// クエリの区切り記号 (&) で文字列を配列に分割する
+			var parameters = query.split('&');
+			var result = new Object();
+			for (var i = 0; i < parameters.length; i++) {
+				// パラメータ名とパラメータ値に分割する
+				var element = parameters[i].split('=');
+				var paramName = decodeURIComponent(element[0]);
+				var paramValue = decodeURIComponent(element[1]);
+				// パラメータ名をキーとして連想配列に追加する
+				result[paramName] = paramValue;
+			}
+			return result;
 		}
-		return result;
+		return null;
 	}
-	return null;
 }
